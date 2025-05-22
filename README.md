a139a0cc0880c2d3a6c5a7ac34334ecaaa20805c
Lee Ellam <lee.ellam@gmail.com>
2025-05-22 09:35:59 +0100

    Reply to emails and ignore non-post requests

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ We are going to build a simple RAG service that adds, lists and searches bookmar
 
 ## Prerequisites
 
-Start off by signing up for a Cloudflare Workers free plan and a Postmark plan.
+Start off by signing up for a Cloudflare Workers free plan and a Postmark plan (make sure you verify this, so you can reply to emails from within the app).
 
 ### Let's go
 
-Start off by creating a new Cloudflare Worker and follow the prompts to make a Hello World worker with Durable Object:
+Start off by creating a new Cloudflare Worker and follow the prompts to make a Hello World worker:
 `npm create cloudflare@latest`
 
 Next, login to your Cloudflare account so that we can deploy our worker, and create resources:
@@ -20,6 +20,65 @@ Now that that is out of the way, let's run it and make sure everything works as 
 
 Visit the URL printed in the console (http://localhost:8787) and we should see Hello, world! We’re off to the races.
 
-Now, let's deploy our Worker to Cloudflare, so we can setup the inbound email server on Postmark.
+Now, let’s deploy our Worker to Cloudflare, so we can setup the inbound email server on Postmark.
 
 `npm run deploy`
+
+Take the URL from that command (ending .workers.dev) and visit it, Hello, world! should be visible once more.
+
+### Interlude: Postmark
+
+At this point, let’s pause, and head over to Postmark to set up email processing. I won’t rehash their documentation... https://postmarkapp.com/developer/user-guide/inbound/configure-an-inbound-server
+
+Make note of the inbound email address.
+
+### Continue
+
+Let’s handle POST requests in our Worker so we can receive an email and reply with the contents.
+
+Grab your Postmark Server Token, and add set it as a secret.
+
+```
+echo POSTMARK_SERVER_TOKEN=YOUR_TOKEN >> .dev.vars
+npx wrangler types
+npx wrangler secret bulk .dev.vars
+```
+
+And edit your `src/index.ts` POST handler to reply to the email
+
+```
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		if (request.method !== 'POST') {
+			return new Response('Not Found', { status: 404 });
+		}
+
+		const message = await request.json();
+		const { To, From, Subject } = message as Record<string, unknown>;
+
+		ctx.waitUntil(
+			fetch('https://api.postmarkapp.com/email', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Postmark-Server-Token': env.POSTMARK_SERVER_TOKEN,
+				},
+				body: JSON.stringify({
+					To: From as string,
+					From: To as string,
+					Subject: 'Message received',
+					TextBody: JSON.stringify(message, null, 2),
+					HtmlBody: `<pre>${JSON.stringify(message, null, 2)}</pre>`,
+				}),
+			})
+		);
+
+		return new Response(null, {
+			status: 204,
+		});
+	},
+} satisfies ExportedHandler<Env>;
+
+```
+
+Redeploy, and send another email - now we should should receive an email reply.
--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,33 @@
-import { DurableObject } from 'cloudflare:workers';
-
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
-
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class Postmarks extends DurableObject<Env> {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 */
-	constructor(ctx: DurableObjectState, env: Env) {
-		super(ctx, env);
-	}
-
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
-	async sayHello(name: string): Promise<string> {
-		return `Hello, ${name}!`;
-	}
-}
+export { Postmarks } from './objects/postmarks';
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
 	async fetch(request, env, ctx): Promise<Response> {
-		// Create a `DurableObjectId` for an instance of the `MyDurableObject`
-		// class named "foo". Requests from all Workers to the instance named
-		// "foo" will go to a single globally unique Durable Object instance.
-		const id: DurableObjectId = env.POSTMARKS.idFromName('foo');
+		if (request.method !== 'POST') {
+			return new Response('Not Found', { status: 404 });
+		}
 
-		// Create a stub to open a communication channel with the Durable
-		// Object instance.
-		const stub = env.POSTMARKS.get(id);
+		const message = await request.json();
+		const { To, From, Subject } = message as Record<string, unknown>;
 
-		// Call the `sayHello()` RPC method on the stub to invoke the method on
-		// the remote Durable Object instance
-		const greeting = await stub.sayHello('world');
+		ctx.waitUntil(
+			fetch('https://api.postmarkapp.com/email', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Postmark-Server-Token': env.POSTMARK_SERVER_TOKEN,
+				},
+				body: JSON.stringify({
+					To: From as string,
+					From: To as string,
+					Subject: 'Message received',
+					TextBody: JSON.stringify(message, null, 2),
+					HtmlBody: `<pre>${JSON.stringify(message, null, 2)}</pre>`,
+				}),
+			})
+		);
 
-		return new Response(greeting);
+		return new Response(null, {
+			status: 204,
+		});
 	},
 } satisfies ExportedHandler<Env>;
